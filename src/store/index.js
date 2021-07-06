945c8a20b1ec702d0c0e6a73b490790f0af18c53
Mpho Maleka <Mpho.Maleka@rheinmetall-denelmunition.com>
2021-07-06 14:34:06 +0200

    change numbers

--- src/store/index.js
+++ src/store/index.js
@@ -17,8 +17,8 @@ export default new Vuex.Store({
     endpoints: {
       obtainJWT: 'api-food_delivery/api/token/',
       refreshJWT: 'api-food_delivery/api/token/refresh/',
-      baseURL: 'http://127.0.0.1:8080/',
-      // baseURL: 'https://try-coding.herokuapp.com/'
+      // baseURL: 'http://127.0.0.1:8080/',
+      baseURL: 'https://try-coding.herokuapp.com/'
     },
     loggedIn: '',
     registered: '',
